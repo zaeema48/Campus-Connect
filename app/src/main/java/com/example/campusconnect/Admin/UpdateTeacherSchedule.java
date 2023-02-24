1dec62368949de9452359c450c4e83d21412553c
Zeeshan <zeeshankalimkhan@gmail.com>
2023-02-24 20:19:45 +0530

    login layot for all users

--- app/src/main/java/com/example/campusconnect/Admin/UpdateTeacherSchedule.java
+++ app/src/main/java/com/example/campusconnect/Admin/UpdateTeacherSchedule.java
@@ -3,13 +3,14 @@ package com.example.campusconnect.Admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import com.example.campusconnect.R;
 
-public class UpdateSchedule extends AppCompatActivity {
+public class UpdateTeacherSchedule extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_schedule);
+        setContentView(R.layout.activity_update_teacher_schedule);
     }
 }
--- app/src/main/java/com/example/campusconnect/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/campusconnect/Fragments/HomeFragment.java
@@ -12,15 +12,15 @@ import android.view.ViewGroup;
 import com.example.campusconnect.R;
 
 public class HomeFragment extends Fragment {
-    CardView addBatch, batchList, searchBatch, updateSem, addStudent, removeStudent, updateStudent, searchStudent, updateSchedule,
-    addTeacher, removeTeacher, searchTeacher, teacherList, updateteacher, subjectList, searchSubject, updateFees, examSchedule;
+    CardView addBatch, batchList, searchBatch, updateSem, addStudent, removeStudent, updateStudent, searchStudent, updateStudentSchedule,
+    addTeacher, removeTeacher, searchTeacher, teacherList, updateTeacher, subjectList, updateTeacherSchedule, updateFees, examSchedule;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-
+//      FETCHING OF VIEWS
         addBatch = view.findViewById(R.id.addBatch);
         batchList = view.findViewById(R.id.batchList);
         searchBatch = view.findViewById(R.id.searchBatch);
@@ -29,14 +29,14 @@ public class HomeFragment extends Fragment {
         removeStudent = view.findViewById(R.id.removeStudent);
         updateStudent = view.findViewById(R.id.updateStudent);
         searchStudent = view.findViewById(R.id.searchStudent);
-        updateSchedule = view.findViewById(R.id.updateSchedule);
+        updateStudentSchedule = view.findViewById(R.id.updateStudentSchedule);
         addTeacher = view.findViewById(R.id.addTeacher);
         removeTeacher = view.findViewById(R.id.removeTeacher);
         searchTeacher = view.findViewById(R.id.searchTeacher);
         teacherList = view.findViewById(R.id.teacherList);
-        updateteacher = view.findViewById(R.id.updateTeacher);
+        updateTeacher = view.findViewById(R.id.updateTeacher);
         subjectList = view.findViewById(R.id.subjectList);
-        searchSubject = view.findViewById(R.id.searchSubject);
+        updateTeacherSchedule= view.findViewById(R.id.updateTeacherSchedule);
         updateFees = view.findViewById(R.id.updateFee);
         examSchedule = view.findViewById(R.id.examSchedule);
 
