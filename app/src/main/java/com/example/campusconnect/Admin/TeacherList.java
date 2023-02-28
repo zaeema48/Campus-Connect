20179f4f5e62f87da886d456ad98db07828a7e49
Zeeshan <zeeshankalimkhan@gmail.com>
2023-03-01 03:09:27 +0530

    Teacher List activity code

--- app/src/main/java/com/example/campusconnect/Admin/TeacherList.java
+++ app/src/main/java/com/example/campusconnect/Admin/TeacherList.java
@@ -1,16 +1,41 @@
 package com.example.campusconnect.Admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.campusconnect.Adapter.TeacherListAdapter;
+import com.example.campusconnect.Models.TeacherModel;
 import com.example.campusconnect.R;
 
+import java.util.ArrayList;
+
 public class TeacherList extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    TeacherListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_list);
+        recyclerView=findViewById(R.id.teacherListRV);
+        //TESTING DATA SET
+        ArrayList<TeacherModel> teachers = new ArrayList<>();
+        teachers.get(0).setTeacherName("Zeeshan");
+        teachers.get(0).setTeacherId(321);
+        teachers.get(0).getSubject().setSubjectName("Java");
+        teachers.get(0).setSalary("90k");
+        teachers.get(1).setTeacherName("Zaeema");
+        teachers.get(1).setTeacherId(322);
+        teachers.get(1).getSubject().setSubjectName("C++");
+        teachers.get(1).setSalary("90k");
+
+        adapter=new TeacherListAdapter(TeacherList.this,teachers);
+        recyclerView.setLayoutManager(new LinearLayoutManager(TeacherList.this));
+        recyclerView.setAdapter(adapter);
+
     }
 }
